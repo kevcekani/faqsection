b9ddc3e0e79973f9a2bc237280260c8035b89d9d
Kevin <kevcekani@gmail.com>
2021-09-09 11:38:37 +0200

    FAQ project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className='container'>
+        <h3>Most common questions visitors usually have</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,21 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Cookies are entirely optional. However, you get a better user experience by allowing what you see fit.',
+  },
+  {
+    id: 2,
+    title: 'How do I change my password?',
+    info:
+      'Click under your profile picture, go to Account Settings and enter your new password.',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'BankID is the unique number that connects your user profile to your bank account number such as IBAN.',
+  }
+]
+export default questions
